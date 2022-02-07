f34522981d93c0511ebbc9e59ad263c459448ef7
jhonata <jhonata_ti@outlook.com>
2022-02-07 15:25:02 -0300

    Commit from another PC

---
+++ src/App.js
@@ -0,0 +1,17 @@
+const App = () => {
+  return(
+    <div>
+
+      <h1>Minha primeira aplicação com React</h1>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+      
+
+    </div>
+  )
+}
+
+export default App;
