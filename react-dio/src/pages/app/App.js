2b09919d0443db706fa6f6e3e2161d6ae5123cf2
jhonatanascimento <jhonata_ti@outlook.com>
2022-02-07 10:05:50 -0300

    addes styled components to app

--- react-dio/src/pages/app/App.js
+++ react-dio/src/pages/app/App.js
@@ -1,14 +1,27 @@
 import styled from 'styled-components';
 import narutoImg from '../../images/naruto.png';
+import { Quotes } from '../../components';
 
 export function App() {
   return (
-    <div>
-      <p role="p">Hello</p>
-      <button>Quote No Jutsu</button>
-      <img src={narutoImg} alt='Naruto with a Kunai'/>
-    </div>
+    <Content>
+     <Quotes quote={"OK"} speaker={"Speaker"}/>
+      <NarutoImg src={narutoImg} alt='Naruto with a Kunai'/>
+    </Content>
   );
 }
+
+const Content = styled.div`
+height: 100vh;
+padding: 0 50px;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+
+const NarutoImg = styled.img`
+max-width:50vw;
+align-self:flex-end;
+`
 
 
